78a6cf1b7a9bbc3f53ef787e484d744d44e32835
alex <alex.86p@yandex.ru>
2018-04-22 08:58:57 +0000

    firt commit

---
+++ gen.sh
@@ -0,0 +1,13 @@
+openssl genrsa -out shashki.online.key 2048
+openssl req -new -key shashki.online.key -out shashki.online.csr
+openssl req -new -out workingbit.csr -config workingbit.conf 
+openssl genrsa -out ca.key 2048
+openssl req -new -x509 -key ca.key -out ca.crt
+openssl x509 -req -in shashki.online.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out shashki.online.crt
+cat shashki.online.crt ca.crt > shashki.online.bundle.crt
+mkdir newcerts
+touch index.txt
+echo '01' > serial
+openssl ca -config ca.conf -out shashki.online.crt -infiles shashki.online.csr 
+openssl ca -config ca.conf -out workingbit.crt -extfile workingbit.extensions.conf -in workingbit.csr
+openssl verify -CAfile ca.crt workingbit.crt
