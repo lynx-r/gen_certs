9b590b3c4454efdca23bd9ddc2d58591dfb09a50
Aleksey <aleksey.popryaduhin@yahoo.com>
2018-04-23 12:36:13 +0000

    update

--- gen.sh
+++ gen.sh
@@ -1,8 +1,8 @@
 openssl genrsa -out shashki.online.key 2048
-openssl req -new -key shashki.online.key -out shashki.online.csr
+openssl req -new -key shashki.online.key -out shashki.online.csr -config shashki.online.conf
 openssl req -new -out workingbit.csr -config workingbit.conf 
 openssl genrsa -out ca.key 2048
-openssl req -new -x509 -key ca.key -out ca.crt
+openssl req -new -x509 -key ca.key -out ca.crt -config caREQ.conf
 openssl x509 -req -in shashki.online.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out shashki.online.crt
 cat shashki.online.crt ca.crt > shashki.online.bundle.crt
 mkdir newcerts
